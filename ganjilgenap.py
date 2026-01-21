6a762ebad30de5ee0d8b6627d8f81522ff48d316
RioArws <ar.razzak101@sma.belajar.id>
2026-01-21 06:01:23 +0000

    versi 1.1 ganjigenap

---
+++ ganjilgenap.py
@@ -0,0 +1,8 @@
+bil = int(input(Masukan angka:))
+
+if bil %2 = 0:
+    print(bil,"Adalah Bilangan Genap")
+else:
+    print(bil,"Adalah Bilangan Gaanjil")
+
+print("Kode ini saya buat dari codespace")
